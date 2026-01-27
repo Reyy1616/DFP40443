de908df49e33a9bc50334302f8b8c9d022c53c46
Reyy1616 <mohamadzulfitry@gmail.com>
2026-01-27 14:28:40 +0800

    commit last part

---
+++ W3/student_portal/login.php
@@ -0,0 +1,12 @@
+<?php include 'includes/header.php'; ?>
+ <h2>Student Login</h2>
+ <form>
+ <label>Username:</label><br>
+ <input type="text" placeholder="Enter ID"><br><br>
+
+ <label>Password:</label><br>
+ <input type="password"><br><br>
+
+ <button type="button">Login</button>
+ </form>
+<?php include 'includes/footer.php'; ?>
